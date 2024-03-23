ab3729bbce6876e6a149ad46c90e4a2d0e25d48a
rittin2803 <rittin2803@gmail.com>
2024-03-24 00:55:28 +0530

    Connected to Database, Created environment variables

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
 const app = express();
+dotenv.config();
+try {
+    mongoose.connect(process.env.MONGO_URL)
+    console.log("Database Connected");
+} catch (error) {
+    console.log("Error connecting to Database");
+}
 
 app.listen(3000, () => {
     console.log("Server is running");
